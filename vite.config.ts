1424a4726655e6614fcb5e944330cf237f759343
arnuramirhan-alt <arnur.amirhan@gmail.com>
2026-06-14 11:27:43 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Arn/', 
+  base: '/Arnur/', // <--- ПОМЕНЯЛИ НА Arnur СЛЕШИ С ОБЕИХ СТОРОН ОБЯЗАТЕЛЬНЫ
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
